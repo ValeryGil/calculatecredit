420678682b519f23ef2d49ba8c01114a6511aa68
ValeryGil <valery.gil@icloud.com>
2023-04-29 20:47:32 +0300

    Changed parameters

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,7 @@
 public class CreditPaymentService {
 
-    public double calculate(int sumCredit, int month, double monthPercent) {
+    public double calculate(int sumCredit, int month, double percentCredit) {
+        double monthPercent = percentCredit / (100 * 12); //Месячная процентная ставка
         double result = sumCredit * ((monthPercent * Math.pow((1 + monthPercent), month)) / (Math.pow((1 + monthPercent), month) - 1));
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,7 @@ public class Main {
         int month = 12; //Количество месяцев до погашения кредита
         double percentCredit = 9.99; //Процент кредита
 
-        double monthPercent = percentCredit / (100 * 12); //Месячная процентная ставка
-        double monthPayment = (int) (service.calculate(sumCredit, month, monthPercent));
+        double monthPayment = (int) (service.calculate(sumCredit, month, percentCredit));
 
         System.out.println("Ежемесячеый платеж = " + Math.round(monthPayment) + " руб.");
         System.out.println("Процентная ставка = " + percentCredit + " %");
